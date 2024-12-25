ea662e76088efd9e90dd91dd9dbc5951abb5366b
Arindam Mukherjee <z5421641@ad.unsw.edu.au>
2024-12-25 11:13:41 +1100

    Updating project compileSdk version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.arindam.androidmemorygame"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.arindam.androidmemorygame"
