ea778d05c9b2e808ad7c57cf5334a3464de33f28
Arindam Mukherjee <z5421641@ad.unsw.edu.au>
2024-12-26 21:08:07 +1100

    Added all the vectors we will be using.

---
+++ app/src/main/java/com/arindam/androidmemorygame/utils/Constants.kt
@@ -0,0 +1,18 @@
+package com.arindam.androidmemorygame.utils
+
+import com.arindam.androidmemorygame.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_android,
+    R.drawable.ic_clock,
+    R.drawable.ic_contact,
+    R.drawable.ic_face,
+    R.drawable.ic_hand,
+    R.drawable.ic_home,
+    R.drawable.ic_music,
+    R.drawable.ic_numbers,
+    R.drawable.ic_plane,
+    R.drawable.ic_school,
+    R.drawable.ic_sleep,
+    R.drawable.ic_work
+)
